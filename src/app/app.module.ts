6f15437db513db4bed0b3904b9ef62b252f4963f
CarlosECorreaH <carlosech06@gmail.com>
2022-11-17 15:13:16 -0500

    mod1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './plantilla/navbar/navbar.component';
+import { BarranavegacionComponent } from './plantilla/barranavegacion/barranavegacion.component';
+import { InicioComponent } from './plantilla/inicio/inicio.component';
+import { PiepaginaComponent } from './plantilla/piepagina/piepagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BarranavegacionComponent,
+    InicioComponent,
+    PiepaginaComponent
   ],
   imports: [
     BrowserModule,
