a1cd32cf9c59152373c3eb900117dc66a18b055d
CarlosECorreaH <carlosech06@gmail.com>
2022-11-17 15:31:13 -0500

    mod2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './plantilla/navbar/navbar.component';
 import { BarranavegacionComponent } from './plantilla/barranavegacion/barranavegacion.component';
 import { InicioComponent } from './plantilla/inicio/inicio.component';
 import { PiepaginaComponent } from './plantilla/piepagina/piepagina.component';
@@ -11,7 +10,6 @@ import { PiepaginaComponent } from './plantilla/piepagina/piepagina.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     BarranavegacionComponent,
     InicioComponent,
     PiepaginaComponent
